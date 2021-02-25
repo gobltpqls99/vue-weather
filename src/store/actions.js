3dcaf3a2f58014c020d26fa1280a35be1ed08e31
gobltpqls99 <gobltpqls99@naver.com>
2021-02-25 11:25:06 +0900

    /

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,14 +2,14 @@ import { getWeather } from '../api/weather-api'
 
 export default {
 	async ACT_DAILY({ commit }, value) {
-		commit('MUT_LOADING', true);
+		if(value) commit('MUT_LOADING', true);
 		commit('MUT_CITY', value);
 		commit('MUT_DAILY', await getWeather(value, 'daily'))
 		//로딩시작
 		commit('MUT_LOADING', false);
 	},
 	async ACT_DAYS({ commit }, value) {
-		commit('MUT_LOADING', true);
+		if(value) commit('MUT_LOADING', true);
 		//로딩나타남
 		commit('MUT_DAYS', await getWeather(value, 'days'))
 		commit('MUT_LOADING', false);
